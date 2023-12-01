06e27ca0404cd90a0735644f93cb07ff272e4d14
MIGUEL ANGEL DEL MORAL MARTINEZ <miguelangeldelmoral@hotmail.com>
2023-12-01 18:16:21 +0100

    Eliminada libreria jgoodies que no se lo que es

--- src/Vistas/PanelDeControl.java
+++ src/Vistas/PanelDeControl.java
@@ -17,7 +17,6 @@ import javax.swing.SwingConstants;
 public class PanelDeControl {
 
 	private JFrame frmBiblioteca;
-	private JTextField txtTituloVentana;
 
 	/**
 	 * Launch the application.
@@ -53,23 +52,14 @@ public class PanelDeControl {
 		SpringLayout springLayout = new SpringLayout();
 		frmBiblioteca.getContentPane().setLayout(springLayout);
 		
-		txtTituloVentana = new JTextField();
-		txtTituloVentana.setHorizontalAlignment(SwingConstants.CENTER);
-		springLayout.putConstraint(SpringLayout.NORTH, txtTituloVentana, 5, SpringLayout.NORTH, frmBiblioteca.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, txtTituloVentana, 0, SpringLayout.WEST, frmBiblioteca.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, txtTituloVentana, 434, SpringLayout.WEST, frmBiblioteca.getContentPane());
-		txtTituloVentana.setText("Panel de Control ");
-		frmBiblioteca.getContentPane().add(txtTituloVentana);
-		txtTituloVentana.setColumns(10);
-		
 		JButton btnConsulta = new JButton("Consulta");
 		springLayout.putConstraint(SpringLayout.WEST, btnConsulta, 45, SpringLayout.WEST, frmBiblioteca.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, btnConsulta, 134, SpringLayout.WEST, frmBiblioteca.getContentPane());
 		frmBiblioteca.getContentPane().add(btnConsulta);
 		
 		JButton btnNuevo = new JButton("Nuevo");
+		springLayout.putConstraint(SpringLayout.NORTH, btnNuevo, 53, SpringLayout.NORTH, frmBiblioteca.getContentPane());
 		springLayout.putConstraint(SpringLayout.NORTH, btnConsulta, 0, SpringLayout.NORTH, btnNuevo);
-		springLayout.putConstraint(SpringLayout.NORTH, btnNuevo, 28, SpringLayout.SOUTH, txtTituloVentana);
 		springLayout.putConstraint(SpringLayout.WEST, btnNuevo, 301, SpringLayout.WEST, frmBiblioteca.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, btnNuevo, -44, SpringLayout.EAST, frmBiblioteca.getContentPane());
 		frmBiblioteca.getContentPane().add(btnNuevo);
